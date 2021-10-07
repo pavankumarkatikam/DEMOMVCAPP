66d9ba41c099324c999f4644ca1dfa979358c61b
Katikam <pavankumar.katikam@accenture.com>
2021-10-07 11:11:55 +0530

    Revert "Revert "second commit""
    This reverts commit 9e919558414cfe95a206e0e39640a2bbedbb50bc.

--- DEMOMVCAPP/Controllers/HomeController.cs
+++ DEMOMVCAPP/Controllers/HomeController.cs
@@ -13,6 +13,10 @@ namespace DEMOMVCAPP.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Comment added
+        /// </summary>
+        /// <returns></returns>
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
